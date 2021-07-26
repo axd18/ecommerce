27f515ab698746ff370568a6822b46d622ba0c3d
adrian <adrianfernandezj@gmail.com>
2021-07-26 19:43:17 -0300

    agregando el Router

--- src/App.js
+++ src/App.js
@@ -1,28 +1,34 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import RegisterScreen from './components/auth/RegisterScreen';
+import LoginScreen from './components/auth/LoginScreen';
 
 function App() {
   return (
+    <Router>
+      <Switch>
+        <Route exact path="/" component={LoginScreen} />
+        <Route exact path="/crear-cuenta" component={RegisterScreen} />
+      </Switch>
+    </Router>
     
-    
-    
-    
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
   );
 }
 
