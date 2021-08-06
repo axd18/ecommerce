b43ad84d4cc63097223cd360ea49f6ecab896325
adrian-axd <adrianfernandezj@hotmail.com>
2021-08-06 06:50:34 -0300

    agregando boton para loguearse con Google

--- src/components/auth/LoginScreen.js
+++ src/components/auth/LoginScreen.js
@@ -67,7 +67,15 @@ const Login = () => {
           >
             Iniciar sesión
           </button>
-          <div className="text-base text-gray-500 mt-3">
+          
+          <button
+            type="submit"
+            className="text-white bg-indigo-500 border-0 py-2 px-8 my-4 focus:outline-none hover:bg-indigo-600 rounded text-lg"
+          >
+            Iniciar sesión con Google
+          </button>
+          
+          <div className="text-base text-gray-500 mt-2">
             Recuperar contraseña
           </div>
           <button
