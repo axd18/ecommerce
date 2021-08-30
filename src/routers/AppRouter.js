cc0fab4d85c4f2cf82322bd6a2e68f8c896646e5
adrian-xd <adrianfernandezj@hotmail.com>
2021-08-29 22:07:09 -0300

    mejorando el orden de las rutas

---
+++ src/routers/AppRouter.js
@@ -0,0 +1,9 @@
+
+
+export const AppRouter = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
---
+++ src/routers/AuthRouter.js
@@ -0,0 +1,9 @@
+
+
+export const AuthRouter = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
