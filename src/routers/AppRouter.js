6e46db97c9fa1efd51bc458553dc5f4165c46f53
unknown <mariandev520>
2021-09-08 10:45:27 -0300

    integrando footer

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { AuthRouter } from './AuthRouter';
+import Footer from '../components/footer/Footer';
 // import { LoginScreen } from '../components/auth/LoginScreen';
 
 export const AppRouter = () => {
@@ -8,6 +9,7 @@ export const AppRouter = () => {
             <div>
                 <Switch>
                     <Route path="/auth" component={ AuthRouter } />
+                    <Route exact path="/" component={Footer} />
                     {/* <Route exact path="/" component={ LoginScreen } /> */}
                 </Switch>
             </div>
