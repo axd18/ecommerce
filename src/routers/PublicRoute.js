7eab6ab69908fd1b85e54b55c327d67e96637b6d
adrian-xd <adrianfernandezj@hotmail.com>
2021-09-08 18:35:20 -0300

    proteccion de rutas ok

--- src/routers/PublicRoute.js
+++ src/routers/PublicRoute.js
@@ -14,7 +14,7 @@ export const PublicRoute = ({
         <Route { ...rest }
             component={ (props) => (
                 ( isAuthenticated )
-                    ? ( <Redirect to="/" /> )
+                    ? ( <Redirect to="/crear-producto" /> )
                     : ( <Component { ...props } /> )
             )}
         
