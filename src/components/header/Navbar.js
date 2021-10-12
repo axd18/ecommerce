8a1a7474fb2eab773760c904244fdbd12fbf7178
adrian-xd <adrianfernandezj@hotmail.com>
2021-10-12 20:56:46 -0300

    agregué el buscador en el navbar

--- src/components/header/Navbar.js
+++ src/components/header/Navbar.js
@@ -17,18 +17,34 @@ const Navbar = () => {
         <div className="flex flex-grow">
           <p>Logo</p>
         </div>
-        <div className="flex flex-grow justify-between"> 
-            <div>
-              <a href="#" className="lg:mr-7">Novedades</a>
-              <a href="#" className="lg:mr-7">Hombre</a>
-              <a href="#" className="lg:mr-7">Mujer</a>
-              <a href="#" className="lg:mr-7">Niña/o</a>
-              <a href="#" className="lg:mr-7">Ofertas</a>
-            </div>
-            <div>
-              <a href="#" className="lg:mr-4">buscar</a>
-              <a href="#">carrito</a>
-            </div>
+        <div className="flex flex-grow justify-between">
+          <div>
+            <a href="#" className="lg:mr-7">
+              Novedades
+            </a>
+            <a href="#" className="lg:mr-7">
+              Hombre
+            </a>
+            <a href="#" className="lg:mr-7">
+              Mujer
+            </a>
+            <a href="#" className="lg:mr-7">
+              Niña/o
+            </a>
+            <a href="#" className="lg:mr-7">
+              Ofertas
+            </a>
+          </div>
+          <div className="flex flex-grow">
+          <div class=" mx-auto text-gray-600">
+        <input class="border-2 border-gray-300 bg-white h-10 px-5 pr-16 rounded-lg text-sm focus:outline-none"
+          type="search" name="search" placeholder="Buscar"/>
+        <button type="submit" class="absolute right-0 top-0 mt-5 mr-4">
+         
+        </button>
+      </div>
+            <a href="#">carrito</a>
+          </div>
         </div>
       </div>
     </nav>
