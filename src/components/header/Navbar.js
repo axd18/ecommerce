dc747a8a2ee078292f49148b884a21c08c8a7e0e
adrian-xd <adrianfernandezj@hotmail.com>
2021-10-19 17:56:40 -0300

    Agregue la dependencia heroicons y el icono del carrito en el navbar

--- src/components/header/Navbar.js
+++ src/components/header/Navbar.js
@@ -43,7 +43,9 @@ const Navbar = () => {
          
         </button>
       </div>
-            <a href="#">carrito</a>
+      <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
+</svg>
           </div>
         </div>
       </div>
